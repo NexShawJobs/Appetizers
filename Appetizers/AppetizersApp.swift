9ef2be7bda48d580d8459f1fad506e36702255c7
NexShawJobs <nex.shaw.jobs@gmail.com>
2023-12-26 12:12:22 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by NebSha on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
